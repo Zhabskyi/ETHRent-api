48bea31ed14853cab3546dc067b6dbc2c6e0de6a
Zhabskyi <poltavagaz@gmail.com>
2019-12-28 17:22:25 -0700

    Add Routes for items and users: Handle user register

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,16 +1,25 @@
 const router = require("express").Router();
 
 module.exports = db => {
-  router.get("/items", (request, response) => {
-    db.query(`SELECT * FROM items`).then(({ rows: interviewers }) => {
-      response.json(
-        interviewers.reduce(
+  router.get("/items", (req, res) => {
+    db.query(`SELECT * FROM items`).then(({ rows: items }) => {
+      res.json(
+        items.reduce(
           (previous, current) => ({ ...previous, [current.id]: current }),
           {}
         )
       );
     });
   });
+
+  // const query = {
+  //   text: 'INSERT INTO items(name, email) VALUES($1, $2)',
+  //   values: ['brianc', 'brian.m.carlson@gmail.com'],
+  // }
+
+  // router.post('/items', (req, res) => {
+  //   item = req.body;
+  // })
 
   return router;
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,56 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const database = require("../db/database.js");
+
 
 module.exports = db => {
-  router.get("/users", (request, response) => {
+  router.get("/users", (req, res) => {
     db.query(
       `
       SELECT * from users;
     `
-    ).then(({ rows: days }) => {
-      response.json(days);
+    ).then(({ rows: users }) => {
+      res.json(users);
     });
+  });
+
+  router.post("/users", async (req, res) => {
+    const user = req.body;
+
+    try {
+      let newUser = await database.getUserByEmail(db, user.email);
+
+      if (newUser.length !== 0) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user.password = await bcrypt.hashSync(user.password, 12);
+
+      newUser = await database.addUser(db, user);
+
+      const payload = {
+        newUser: {
+          id: newUser[0].id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   });
 
   return router;
